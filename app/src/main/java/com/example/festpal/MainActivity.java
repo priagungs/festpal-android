fbdf15e37affdf6adc303b6076bf1637afaac937
luthfihadiana <luthfihadiana@gmail.com>
2019-03-29 12:54:47 +0700

    add bar layout and fragment

--- app/src/main/java/com/example/festpal/MainActivity.java
+++ app/src/main/java/com/example/festpal/MainActivity.java
@@ -3,42 +3,81 @@ package com.example.festpal;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CustomViewPager viewPager;
     private TextView mTextMessage;
-
+    private MenuItem prevMenuItem;
+    private Booked bookedFragment;
+    private Explore exploreFragment;
+    private Profile profileFragment;
+    BottomNavigationView navigation;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_explore:
+                    viewPager.setCurrentItem(0);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_booked:
+                    viewPager.setCurrentItem(1);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    viewPager.setCurrentItem(2);
                     return true;
             }
             return false;
         }
     };
+    private ViewPager.OnPageChangeListener mViewPagerChangeListener= new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
 
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            if(prevMenuItem != null){
+                prevMenuItem.setChecked(false);
+            }else{
+                navigation.getMenu().getItem(0).setChecked(false);
+            }
+            navigation.getMenu().getItem(position).setChecked(true);
+            prevMenuItem = navigation.getMenu().getItem(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        exploreFragment = new Explore();
+        bookedFragment = new Booked();
+        profileFragment = new Profile();
+        viewPagerAdapter.addFragment(exploreFragment);
+        viewPagerAdapter.addFragment(bookedFragment);
+        viewPagerAdapter.addFragment(profileFragment);
+        viewPager.setAdapter(viewPagerAdapter);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        viewPager = (CustomViewPager) findViewById(R.id.viewpager);
+        viewPager.setPagingEnabled(false);
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager.addOnPageChangeListener(mViewPagerChangeListener);
+        setupViewPager(viewPager);
     }
 
 }
